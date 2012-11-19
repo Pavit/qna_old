e405471a71bd58a6502b80ebd6e9370184839008
Pavit <pavitm@gmail.com>
2012-11-19 04:45:58 -0500

    heroku push

---
+++ qna/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    url(r'^$', 'core.views.index', name='index'),
+    # url(r'^qna/', include('qna.foo.urls')),
+	url(r'', include('social_auth.urls')),
+	url(r'^questions/$', 'core.views.questions', name='questions'),
+	url(r'^vote/(?P<answer_id>\d+)/$', 'core.views.vote', name='vote'),
+    url(r'^previous_question/(?P<previous_question_pk>\d+)/$', 'core.views.previous_question', name='previous_question'),
+    url(r'^current_question/(?P<current_question_pk>\d+)/$', 'core.views.current_question', name='current_question'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
